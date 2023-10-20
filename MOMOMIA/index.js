32279f6a65bdbf552a7f60cda6ec2c1949e7bec9
Rishika Garg <gargrishika50@gmail.com>
2023-10-20 14:18:26 +0530

    Update index.js
    Updated the contact details for the site

--- MOMOMIA/index.js
+++ MOMOMIA/index.js
@@ -264,6 +264,6 @@ function openWhatsapp() {
     }
 
     let wTxtEncoded = encodeURI(wTxt);
-    window.open("https://wa.me/917428789065?text=" + wTxtEncoded);
+    window.open("https://wa.me/9174287890xx?text=" + wTxtEncoded);
   }
 }
